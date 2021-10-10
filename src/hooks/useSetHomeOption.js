39bb1f6db7f1aa8cf81bdf49c9bfdeebe486400a
Mateo Álvarez <alvarez.fing@gmail.com>
2021-10-10 18:56:19 -0300

    create functions of menu

--- src/hooks/useSetHomeOption.js
+++ src/hooks/useSetHomeOption.js
@@ -1,13 +1,12 @@
 const { HOME_VALUES } = require('../config/valueOfChoices')
+const { useSearchPlace } = require('./useSearchPlace')
 
 const useSetHomeOption = async (option) => {
   return await listOfFunctions[option]()
 }
 
 const listOfFunctions = {
-  [HOME_VALUES.SEARCH]: () => {
-    console.log('hola mundo')
-  },
+  [HOME_VALUES.SEARCH]: useSearchPlace,
   [HOME_VALUES.HISTORY]: () => {},
 }
 
