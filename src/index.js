7afd989e63b29f3878ee0dd32eaa448e68869091
Mateo Álvarez <alvarez.fing@gmail.com>
2021-10-11 17:13:36 -0300

    create a little pause before the functions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const { HOME_VALUES } = require('./config/valueOfChoices')
 const { useSetHomeOption } = require('./hooks/useSetHomeOption')
 const { setHomeMenu } = require('./inquirers/home')
+const { setPause } = require('./inquirers/pause')
 
 const startApp = async () => {
+  console.clear()
   const option = await setHomeMenu()
   if (option === HOME_VALUES.CANCEL) return
   await useSetHomeOption(option)
+  await setPause()
   startApp()
 }
 
